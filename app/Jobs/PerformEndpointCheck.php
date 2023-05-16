feaf2fbaa754cc4f44f0551694ddd4d5a7d29946
abdhadj <abdhadjabdhadj@gmail.com>
2023-05-16 17:51:41 +0100

    Add preform check Job

---
+++ app/Jobs/PerformEndpointCheck.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Endpoint;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+
+class PerformEndpointCheck implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Endpoint $endpoint)
+    {
+        //
+    }
+
+    public function uniqueId()
+    {
+        return 'endpoint_' . $this->endpoint->id;
+    }
+
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+            $response = Http::get($this->endpoint->url());
+        } catch (Exception $e) {
+            //
+        }
+
+        $this->endpoint->checks()->create([
+            'response_code' => $response->status(),
+            'response_body' => !$response->successful() ? $response->body() : null
+        ]);
+
+        $this->endpoint->update([
+            'next_check' => now()->addSeconds($this->endpoint->frequency)
+        ]);
+    }
+}
